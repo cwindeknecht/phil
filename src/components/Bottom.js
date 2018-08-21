604d6018763da5d2c66cab8c86199c4672b589a7
Cody Windeknecht <cmwindeknecht@gmail.com>
2018-08-21 15:53:48 -0700

    Updated So Room 1 Placement Works
    Room 1, the simplest to place, has been worked out.

--- src/components/Bottom.js
+++ src/components/Bottom.js
@@ -6,17 +6,65 @@ import '../css/Bottom.css';
 import parchment from '../imgs/parchment.jpg';
 
 var parchmentBackground = {
-  backgroundImage: `url(${parchment})`
+  backgroundImage: `url(${parchment})`,
 };
 
 class Bottom extends Component {
+  state = {
+    objects: this.props.currentRoom.objects,
+    coords: {
+      x: 0,
+      y: 0,
+    },
+    width: 0,
+  };
+
+  componentDidMount = () => {
+    this.handleSetup();
+    window.addEventListener('resize', this.handleSetup.bind(this));
+
+    this.getObjectsXY();
+  };
+
   render() {
+    // Print the mouse coordinates
+    // function printMousePos(event) {
+    //   console.log('clientX: ' + event.clientX + ' - clientY: ' + event.clientY);
+    // }
+    // document.addEventListener('click', printMousePos);
     return (
-      <div style={ parchmentBackground } className="BottomContainer">
-        {this.props.currentRoom.name}
+      <div id="Container" style={parchmentBackground} className="BottomContainer">
+        {this.state.objects.map((item) => {
+          return (
+            <img
+              key={item.id}
+              src={item.image}
+              style={{ position: 'absolute', left: item.x, top: item.y}}
+            />
+          );
+        })}
       </div>
     );
   }
+
+  getObjectsXY = () => {
+    let main
+    let objects = this.state.objects.map((obj) => {
+      if (typeof obj.x !== 'number') {
+        let update = this.state.objects.filter((object) => {
+          return object.name === obj.x;
+        })[0];
+        return { ...obj, x: update.related.x, y: update.related.y + this.state.coords.y};
+      }
+      return obj;
+    });
+    this.setState({ objects });
+  };
+
+  handleSetup = (visible) => {
+    let coords = document.getElementById('Container').getBoundingClientRect();
+    this.setState({ coords: { x: coords.x, y: coords.y }, width: coords.width });
+  };
 }
 
 const mapStateToProps = (state) => {
--- src/reducers/rooms.js
+++ src/reducers/rooms.js
@@ -1,5 +1,46 @@
-let room1 = {
-    name: 'Room1'
-}
+import mound from '../imgs/BurialMound.png';
+import doorway from '../imgs/clickables/NormalDoorway.png';
 
-module.exports = {room1}
+export let room1 = {
+  name: 'outside',
+  initialTopBar: 'You approach the burial mound.  What do you do?',
+  objects: [
+    {
+      id: 1,
+      name: 'mound',
+      image: mound,
+      background: true,
+      // x position from left of container
+      x: 0,
+      // y position from top of container
+      y: 0,
+      clicked: null,
+      related: {
+        x: 525,
+        y: 377,
+      },
+    },
+    {
+      id: 2,
+      name: 'doorway',
+      image: doorway,
+      background: false,
+      x: 'mound',
+      y: 'mound',
+      // what happens on click
+      clicked: {
+        text: 'Go inside?',
+        yes: {
+          travel: true,
+          location: 'golem',
+        },
+        no: null,
+      },
+      related: null,
+    },
+  ],
+};
+
+let room2 = {
+  name: 'golem',
+};
